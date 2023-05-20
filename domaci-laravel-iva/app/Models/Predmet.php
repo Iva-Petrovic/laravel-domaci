d5c22bfaf5530e2a7b0231dab882e777494a5388
Iva-Petrovic <ivapet98@gmail.com>
2023-05-21 01:12:54 +0200

    Modeli, seeder i factory postavljeni

---
+++ domaci-laravel-iva/app/Models/Predmet.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Predmet extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function ispits()
+    {
+        //one-to-many
+        return $this->hasMany(Ispit::class);
+    }
+}
---
+++ domaci-laravel-iva/database/factories/PredmetFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Predmet>
+ */
+class PredmetFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'Naziv' => $this->faker->sentence($nbWords = 1, $variableNbWords = true),
+            'Opis' => $this->faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+            'Naziv katedre' => $this->faker->sentence($nbWords = 1, $variableNbWords = true),
+        ];
+    }
+}
