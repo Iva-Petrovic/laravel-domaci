35cc459fe573a61bdd7bb7061122de7b2e6b4511
Iva-Petrovic <ivapet98@gmail.com>
2023-05-21 02:38:10 +0200

    Implementacija autentifikovanja

--- domaci-laravel-iva/routes/api.php
+++ domaci-laravel-iva/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IspitController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,16 +17,25 @@ use App\Http\Controllers\StudentController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+
+    Route::resource('students', StudentController::class);
+    Route::post('/students', [StudentController::class, 'addStudent']);
+    Route::delete('/students/{student}', [StudentController::class, 'deleteStudent']);
+
+    Route::resource('ispits', IspitController::class);
+    Route::post('/ispits', [IspitController::class, 'addIspit']);
+    Route::delete('/ispits/{ispit}', [IspitController::class, 'deleteIspit']);
+
 });
 
-Route::resource('students', StudentController::class);
 Route::get('/students', [StudentController::class, 'getAllStudents']);
-Route::post('/students', [StudentController::class, 'addStudent']);
-Route::delete('/students/{student}', [StudentController::class, 'deleteStudent']);
 
-Route::resource('ispits', IspitController::class);
 Route::get('/ispits', [IspitController::class, 'getAllIspits']);
-Route::post('/ispits', [IspitController::class, 'addIspit']);
-Route::delete('/ispits/{ispit}', [IspitController::class, 'deleteIspit']);
